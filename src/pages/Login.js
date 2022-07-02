ec18ab7f0821d34c586ef2114cb7bb46aea549b2
suryaa6666 <suryaelidanto@gmail.com>
2022-07-02 16:52:24 +0700

    done

---
+++ src/pages/Login.js
@@ -0,0 +1,44 @@
+import { Col, Container, Row, Form, Button, Image } from 'react-bootstrap';
+import loginStyle from '../styles/Login.module.css';
+
+const Login = () => {
+    return (
+        <Container className="d-flex align-items-center justify-content-center" style={{ height: '100vh' }}>
+            <Row>
+                <Col sm={6} style={{ marginBottom: '30px', marginTop: '100px' }}>
+                    <Image src='./assets/img/logo.png' style={{ maxWidth: '200px', maxHeight: '200px', heigt: '100%', width: '100%' }} alt='Image cannot be loaded!' fluid={true} />
+                    <div className="mt-4">
+                        <h1 style={{ color: '#FFF', fontWeight: 'bold' }}> Easy, Fast and Reliable </h1>
+                        <p style={{ color: '#6A6A6A' }}>Go shopping for merchandise, just go to dumb merch shopping. the biggest merchandise in Indonesia</p>
+                    </div>
+                    <Row className="mt-5">
+                        <Col sm={6}>
+                            <Button variant="danger" className="pe-3 ps-3 mx-2">Login</Button>
+                            <Button variant="outline-secondary" className="pe-3 ps-3 mx-2">Register</Button>
+                        </Col>
+                        <Col sm={6}>
+                        </Col>
+                    </Row>
+                </Col>
+                <Col sm={6} className="d-flex align-items-center">
+                    <Container style={{ backgroundColor: '#191819', padding: '30px', borderRadius: '10px' }}>
+                        <h1 className={loginStyle.headerTitle}>Login</h1>
+                        <Form className="mt-4">
+                            <Form.Group className="mb-3">
+                                <Form.Control type="email" placeholder="Email" style={{ backgroundColor: '#474647' }} />
+                            </Form.Group>
+                            <Form.Group className="mb-3">
+                                <Form.Control type="password" placeholder="Password" style={{ backgroundColor: '#474647' }} />
+                            </Form.Group>
+                            <Button variant="danger" type="submit" className="w-100 mt-4">
+                                Login
+                            </Button>
+                        </Form>
+                    </Container>
+                </Col>
+            </Row>
+        </Container>
+    )
+}
+
+export default Login
