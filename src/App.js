6afbad3ae8352c2f75d9fc45260a793d2b93faf5
suryaa6666 <suryaelidanto@gmail.com>
2022-07-02 17:10:02 +0700

    done

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '../src/App.css';
 import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
     <>
-      <Login />
+      <Register />
     </>
   );
 }
